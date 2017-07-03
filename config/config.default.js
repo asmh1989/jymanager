3d9336cfc00db4d247a9dc8cdb0eccad7c9791fb
sun <asmh1989@gmail.com>
2017-07-03 10:39:33 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1499049309288_3975';
+
+  // add your config here
+
+  return config;
+};
